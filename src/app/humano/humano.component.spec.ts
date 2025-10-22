dbd99a99be802f9342312d84d285d7c40266eaef
Horacio Aranda <horacio.aranda.py@gmail.com>
2025-10-22 11:22:57 -0300

    Primer commit tqm fede

---
+++ src/app/humano/humano.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HumanoComponent } from './humano.component';
+
+describe('HumanoComponent', () => {
+  let component: HumanoComponent;
+  let fixture: ComponentFixture<HumanoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HumanoComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HumanoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/humano/humano.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-humano',
+  imports: [],
+  templateUrl: './humano.component.html',
+  styleUrl: './humano.component.css'
+})
+export class HumanoComponent {
+
+}
